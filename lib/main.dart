b3e253610adbc50ac966627c3d64b48b222fa403
dhimaseka <dimaseka83@gmail.com>
2022-03-17 12:25:44 +0700

    ++

--- lib/main.dart
+++ lib/main.dart
@@ -22,76 +22,9 @@ class FirstScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('First Screen'),
       ),
-      body: Column(
-        children: <Widget>[
-          Padding(padding: EdgeInsets.only(top: 10)),
-          Text('MainAxisAlignment.spaceEvenly'),
-          Padding(padding: EdgeInsets.only(top: 10)),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              Icon(Icons.share),
-              Icon(Icons.thumb_up),
-              Icon(Icons.thumb_down)
-            ],
-          ),
-          Padding(padding: EdgeInsets.only(top: 10)),
-          Text('MainAxisAlignment.spaceAround'),
-          Padding(padding: EdgeInsets.only(top: 10)),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: <Widget>[
-              Icon(Icons.share),
-              Icon(Icons.thumb_up),
-              Icon(Icons.thumb_down)
-            ],
-          ),
-          Padding(padding: EdgeInsets.only(top: 10)),
-          Text('MainAxisAlignment.spaceBetween'),
-          Padding(padding: EdgeInsets.only(top: 10)),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              Icon(Icons.share),
-              Icon(Icons.thumb_up),
-              Icon(Icons.thumb_down)
-            ],
-          ),
-          Padding(padding: EdgeInsets.only(top: 10)),
-          Text('MainAxisAlignment.start'),
-          Padding(padding: EdgeInsets.only(top: 10)),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-              Icon(Icons.share),
-              Icon(Icons.thumb_up),
-              Icon(Icons.thumb_down)
-            ],
-          ),
-           Padding(padding: EdgeInsets.only(top: 10)),
-          Text('MainAxisAlignment.center'),
-          Padding(padding: EdgeInsets.only(top: 10)),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Icon(Icons.share),
-              Icon(Icons.thumb_up),
-              Icon(Icons.thumb_down)
-            ],
-          ),
-           Padding(padding: EdgeInsets.only(top: 10)),
-          Text('MainAxisAlignment.end'),
-          Padding(padding: EdgeInsets.only(top: 10)),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: <Widget>[
-              Icon(Icons.share),
-              Icon(Icons.thumb_up),
-              Icon(Icons.thumb_down)
-            ],
-          ),
-        ],
-      ),
-    );
+      body: IconButton(onPressed: () {}, 
+        icon: Icon(Icons.volume_up),
+        tooltip: 'Increase your volume',
+      )
   }
 }
