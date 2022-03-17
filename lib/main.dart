d4a724202d87e6c6b129249199be7aabbf6fab14
dhimaseka <dimaseka83@gmail.com>
2022-03-17 09:54:33 +0700

    ++

--- lib/main.dart
+++ lib/main.dart
@@ -4,27 +4,94 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue
-      ),
+      theme: ThemeData(primarySwatch: Colors.blue),
       home: FirstScreen(),
     );
   }
 }
 
-class FirstScreen extends StatelessWidget{
+class FirstScreen extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('First Screen'),
       ),
-      body: Padding(padding: EdgeInsets.all(30), child: Text('Ini Padding'),)
+      body: Column(
+        children: <Widget>[
+          Padding(padding: EdgeInsets.only(top: 10)),
+          Text('MainAxisAlignment.spaceEvenly'),
+          Padding(padding: EdgeInsets.only(top: 10)),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Icon(Icons.share),
+              Icon(Icons.thumb_up),
+              Icon(Icons.thumb_down)
+            ],
+          ),
+          Padding(padding: EdgeInsets.only(top: 10)),
+          Text('MainAxisAlignment.spaceAround'),
+          Padding(padding: EdgeInsets.only(top: 10)),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              Icon(Icons.share),
+              Icon(Icons.thumb_up),
+              Icon(Icons.thumb_down)
+            ],
+          ),
+          Padding(padding: EdgeInsets.only(top: 10)),
+          Text('MainAxisAlignment.spaceBetween'),
+          Padding(padding: EdgeInsets.only(top: 10)),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Icon(Icons.share),
+              Icon(Icons.thumb_up),
+              Icon(Icons.thumb_down)
+            ],
+          ),
+          Padding(padding: EdgeInsets.only(top: 10)),
+          Text('MainAxisAlignment.start'),
+          Padding(padding: EdgeInsets.only(top: 10)),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Icon(Icons.share),
+              Icon(Icons.thumb_up),
+              Icon(Icons.thumb_down)
+            ],
+          ),
+           Padding(padding: EdgeInsets.only(top: 10)),
+          Text('MainAxisAlignment.center'),
+          Padding(padding: EdgeInsets.only(top: 10)),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Icon(Icons.share),
+              Icon(Icons.thumb_up),
+              Icon(Icons.thumb_down)
+            ],
+          ),
+           Padding(padding: EdgeInsets.only(top: 10)),
+          Text('MainAxisAlignment.end'),
+          Padding(padding: EdgeInsets.only(top: 10)),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: <Widget>[
+              Icon(Icons.share),
+              Icon(Icons.thumb_up),
+              Icon(Icons.thumb_down)
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
